a1e9e72ab96468a852c6505264bb6ffe9d688e88
Ahmet Sarıgüney <36050511+Yhellosaud@users.noreply.github.com>
2019-12-24 15:03:28 +0300

    Create Coach.java

---
+++ Model/Coach.java
@@ -0,0 +1,21 @@
+import Model.*;
+
+public class Coach {
+    private String form;
+    private GamePlan gamePlan;
+    public Coach() {
+        form = "";
+    }
+    public Coach(String form) {
+        this.form = form;
+    }
+    public void setForm(String form) {
+        this.form = form;
+    }
+    public String getForm() {
+        return form;
+    }
+    public void setGamePlan(GamePlan gamePlan) {
+        this.gamePlan = gamePlan;
+    }
+}
